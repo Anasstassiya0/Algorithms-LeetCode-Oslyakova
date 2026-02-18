3d36e78b7c6411161ba38d292298090b79d61877
Anastassiya Oslyakova <oslyakovaanastassiya@gmail.com>
2026-02-19 00:30:47 +0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (56.41%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,19 +1,19 @@
 class Solution:
     def search(self, nums, target):
-        return self.binary(nums, target, 0, len(nums) - 1)
+        left = 0
+        right = len(nums) - 1
 
-    def binary(self, nums, target, left, right):
-        if left > right:
-            return -1
+        while left <= right:
+            mid = (left + right) // 2
 
-        mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
 
-        if nums[mid] == target:
-            return mid
-        if target < nums[mid]:
-            return self.binary(nums, target, left, mid - 1)
-        return self.binary(nums, target, mid + 1, right)
-
+        return -1
 
 nums = [-1, 0, 3, 5, 9, 12]
 print(Solution().search(nums, 9))
