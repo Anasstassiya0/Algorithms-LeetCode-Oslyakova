57a59b974f620eb4baacccec1340bf8b514b91e2
Anastassiya Oslyakova <oslyakovaanastassiya@gmail.com>
2026-03-02 22:09:23 +0500

    Add files via upload
    Напишите функцию quick_sort(arr, low, high), которая сортирует массив arr на месте (in place) с использованием рандомизированного выбора опорного элемента.

---
+++ randomixed_QuickSelect.py
@@ -0,0 +1,26 @@
+import random
+def partition(arr, low, high):  # разделение Lomuto
+    pivot = arr[high] # опорный элемент
+    i = low - 1 # индекс меньших элементов
+    for j in range(low, high):
+        if arr[j] <= pivot: # если текущий элемент меньше или равен pivot
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]  # меняем местами
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+def quickselect(arr, low, high, k):
+    if low <= high:
+        pivot_index = random.randint(low, high) #случайный pivot
+        arr[pivot_index], arr[high] = arr[high], arr[pivot_index]
+        pivot_pos = partition(arr, low, high)
+        if pivot_pos == k:
+            return arr[pivot_pos]
+        elif k < pivot_pos: # ищем в левой части
+            return quickselect(arr, low, pivot_pos - 1, k)
+        else: # ищем в правой части
+            return quickselect(arr, pivot_pos + 1, high, k)
+num = [9, 3, 7, 1, 8, 2, 5]
+k = 3
+result = quickselect(num, 0, len(num) - 1, k)
+print("Массив:", num)
+print(f"{k+1}-й самый маленький элемент:", result)
